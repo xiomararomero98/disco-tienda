44405e165aec90f1ad6060b2dbd42518feae9dbb
xiomararomero98 <xio.romero@duocuc.cl>
2025-09-26 15:47:16 -0300

    menu hamnburguesa media

---
+++ assets/js/navbar.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.getElementById("menu-toggle");
+  const navLinks = document.getElementById("nav-links");
+
+  if (toggle && navLinks) {
+    toggle.addEventListener("click", () => {
+      navLinks.classList.toggle("show");
+    });
+  }
+});
